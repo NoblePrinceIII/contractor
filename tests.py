374db63ecc6df097bc65ad630b36d5849dd54f39
nobleprinceiii <farhan.begg@students.makeschool.com>
2019-10-10 00:47:39 -0700

    added test

--- tests.py
+++ tests.py
@@ -3,6 +3,20 @@
 from unittest import TestCase, main as unittest_main
 from app import app
 
+
+sample_listing_id = ObjectId('5d55cffc4a3d4031f42827a3')
+sample_listing = {
+    'title': 'Red Hoodie',
+    'price': '100',
+    'image': 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQnpUskOPTmPAPOXNYDaBpZsgdogqXKqCtiqq_V5Hqxq6WBFcBY62z6aOKPHP4crqEJz8nX9a3IIMgj3789RNhVPRtSwxqeNSz6PG4U7HRNSgViqzd_DwkTMg&usqp=CAY'
+}
+sample_form_data = {
+    'title': sample_listing['title'],
+    'price': sample_listing['price'],
+    'image': sample_listing['image']
+}
+
+
 class HoodieTest(TestCase):
     """Flask tests."""
 
@@ -12,19 +26,17 @@ class HoodieTest(TestCase):
         self.assertEqual(result.status, '200 OK')
         self.assertIn(b'Hoodie', result.data)
 
-        def test_new(self):
-            """Test the new playlist creation page."""
-            result = self.client.get('/red')
-            self.assertEqual(result.status, '200 OK')
-            self.assertIn(b'New Hoodie', result.data)
-
+    def test_new(self):
+        """Test the new playlist creation page."""
+        result = self.client.get('/red')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'New Hoodie', result.data)
 
     def setUp(self):
         """Stuff to do before every test."""
 
         # Get the Flask test client
         self.client = app.test_client()
-
         # Show Flask errors that happen during tests
         app.config['TESTING'] = True
 
